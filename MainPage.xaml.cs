8657e8422dd27e23d48341a6b0a8464b51a8459a
Murillo2999 <murillo25.martins@hotmail.com>
2024-09-06 21:59:34 -0300

    Mensagem de atualização

---
+++ MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace DiceRoller;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void 
+      
+
+        
+        
+        
+
+    
+    //Sortear um número aleatório através do clique do botão
+    //Devolver o número sorteado para a interface
+    //Conseguir selecionar a quantidade de lados a partir do Picker
+
+}
+
